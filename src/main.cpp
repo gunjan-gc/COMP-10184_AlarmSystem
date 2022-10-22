37cad9a92aee2ec7040fedf05c02bdb91aedd4f4
Gunjan <gunjanchaudhary2000@gmail.com>
2022-10-21 23:53:23 -0400

    project 1

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+// COMP-10184 – Mohawk College 
+// PIR Sensor Test Application 
+// Turn on the blue LED with motion is detected. 
+ 
+
+//  I Gunjan Chaudhary, 000831804 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+ //Stage 4 completed
+#include <Arduino.h> 
+ 
+// digital input pin definitions 
+#define PIN_PIR D5 
+#define PIN_BUTTON D6 
+ 
+ int buttonState =0;            //initially the led is set to off
+ const int quarterBlinkSec = 125;
+ bool oldButtonState = LOW;
+//  bool prevButtonState = LOW;
+
+//this state is used to keep a track if the PIR sensor has already sensed motion before, then it has to remain turned on and not detect any motion 2nd time
+int lightState = HIGH;     
+void setup() { 
+  Serial.begin(115200);           // configure the USB serial monitor 
+  
+  pinMode(LED_BUILTIN, OUTPUT);          // configure the LED output 
+ 
+ pinMode(PIN_PIR, INPUT);               // PIR sensor is an INPUT 
+ 
+  pinMode(PIN_BUTTON, INPUT_PULLUP);        // Button is an INPUT 
+  
+  digitalWrite(LED_BUILTIN, HIGH);   //keeping led off in the beginning
+
+
+} 
+
+void loop() { 
+  bool bPIR; 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  bPIR = digitalRead(PIN_PIR); 
+  buttonState = digitalRead(D5);   //Reading button state
+ 
+ //loop only runs if the button is in on state,  if not it does not fo blinking and if in -between the button is pressed, it stops the loop
+while(digitalRead(D5)!= LOW){ 
+//if mothion deteccted and it is the first time when motion is gtting detected, then start blinking
+  if(bPIR == HIGH && lightState == HIGH){
+  // digitalWrite(LED_BUILTIN, !bPIR);
+
+  for(int i =0; i< 40; i++)     //loop till 40 because 4 times per sec * 10 sec
+  {
+    digitalWrite(LED_BUILTIN, !bPIR);     //turn on led
+    delay(quarterBlinkSec);               //delay for 125 ms because we have to blink 4 times per second
+    digitalWrite(LED_BUILTIN, bPIR);      //turn off led
+    delay(quarterBlinkSec);               //delay of 125 ms
+  }
+  lightState = LOW;                     //state to keep a recoed that motion has already been detected 1 time
+  digitalWrite(LED_BUILTIN, !bPIR);   //keep it on after blink is over
+  }
+  //if motion has been detected before, then keep light turned on
+  else if(lightState== HIGH){
+    digitalWrite(LED_BUILTIN, !bPIR);
+  }
+}
+}
